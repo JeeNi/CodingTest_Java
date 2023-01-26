1e1e27d91afb02fd398f4c77306eda2ee95e8f97
JeeNi <parkej9494@gmail.com>
2023-01-26 17:57:32 +0900

    P11660

--- src/test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class test {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int score = sc.nextInt();
-    }
-}
