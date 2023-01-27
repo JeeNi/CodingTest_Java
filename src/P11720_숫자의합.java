85a2d699331e8fa4839ad5627fa80af65030868d
JeeNi <parkej9494@gmail.com>
2023-01-27 17:50:48 +0900

    P1546 슈도코드 작성

--- src/P11720_숫자의합.java
+++ src/P11720_숫자의합.java
@@ -6,9 +6,10 @@ public class P11720_숫자의합 {
         int num = sc.nextInt();                         // num 값 입력받기
         String sNum = sc.next();                        // 길이 num의 숫자를 입력받아 String형 변수 sNum에 저장하기
         char[] cNum = sNum.toCharArray();               // sNum을 다시 char[]형 변수 cNum에 변환하여 저장하기
-        int sum = 0;                                    // int형 변수 sum 선언하기
+        int sum = 0;                                    // int형 변수 sum 선언하기(초기화)
         for (int i = 0; i < cNum.length; i++) {         // for(cNum 길이만큼 반복하기)
             sum += cNum[i] - '0';                       // 배열의 index 값을 정수형(아스키코드 사용)으로 변환하여 sum에 더하여 누적하기
+                                                        // 문자열 '0'을 빼주거나, 49를 빼주기
         }
         System.out.println(sum);                        // sum 출력하기
     }
--- src/P1546_평균.java
+++ src/P1546_평균.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class P1546_평균 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int A[] = new int[N];
+        int N = sc.nextInt();                           // 변수 N에 과목의 수 입력받기
+        int A[] = new int[N];                           // 길이가 N인 1차원 배열 A[] 선언하기
 
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+        for (int i = 0; i < N; i++) {                   // for(A[] 길이만큼 반복하기)
+            A[i] = sc.nextInt();                        // A[i]에 각 점수 저장하기 
         }
 
-        long max = 0;
-        long sum = 0;
-        for (int i = 0; i < N; i++) {
-            if (A[i] > max) max = A[i];
+        long max = 0;                                   // long 타입의 max 변수 선언하기
+        long sum = 0;                                   // long 타입의 sum 변수 선언하기
+        for (int i = 0; i < N; i++) {                   // for(A[] 길이만큼 반복하기)
+            if (A[i] > max) max = A[i];                 // 최고점은 변수 max에, 총점은 변수 sum에 저장하기
             sum += A[i];
         }
-        System.out.println(sum * 100.0 / max / N);
+        System.out.println(sum * 100.0 / max / N);      // sum * 100 / max / N 출력하기
     }
 }
