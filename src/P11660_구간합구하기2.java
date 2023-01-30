de236070144156c535584381ddbc4c01f894810d
JeeNi <parkej9494@gmail.com>
2023-01-30 15:52:10 +0900

    P11660 슈도코드 작성

--- src/P11660_구간합구하기2.java
+++ src/P11660_구간합구하기2.java
@@ -10,30 +10,25 @@ public class P11660_구간합구하기2 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        System.out.println("N " + N);
-        System.out.println("M " + M);
+        int N = Integer.parseInt(st.nextToken());               // 배열 크기 저장하기
+        int M = Integer.parseInt(st.nextToken());               // 질의 수 저장하기
 
         int A[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {                          // N만큼 반복하기
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+                A[i][j] = Integer.parseInt(st.nextToken());     // 원본 배열 저장하기
             }
         }
 
-        System.out.println(Arrays.deepToString(A));
-
-        int D[][] = new int[N + 1][N + 1];
+        int D[][] = new int[N + 1][N + 1];                      // N만큼 반복하기
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
+            for (int j = 1; j <= N; j++) {                      // 합 배열 저장하기
                 D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
             }
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {                           // M만큼 반복하기
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
--- src/P1546_평균.java
+++ src/P1546_평균.java
@@ -4,17 +4,21 @@ public class P1546_평균 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();                           // 변수 N에 과목의 수 입력받기
-        int A[] = new int[N];                           // 길이가 N인 1차원 배열 A[] 선언하기
+//        int A[] = new int[N];                           // 길이가 N인 1차원 배열 A[] 선언하기
 
-        for (int i = 0; i < N; i++) {                   // for(A[] 길이만큼 반복하기)
-            A[i] = sc.nextInt();                        // A[i]에 각 점수 저장하기 
-        }
+//        for (int i = 0; i < N; i++) {                   // for(A[] 길이만큼 반복하기)
+//            A[i] = sc.nextInt();                        // A[i]에 각 점수 저장하기
+//        }
 
         long max = 0;                                   // long 타입의 max 변수 선언하기
         long sum = 0;                                   // long 타입의 sum 변수 선언하기
         for (int i = 0; i < N; i++) {                   // for(A[] 길이만큼 반복하기)
-            if (A[i] > max) max = A[i];                 // 최고점은 변수 max에, 총점은 변수 sum에 저장하기
-            sum += A[i];
+            int temp = sc.nextInt();
+            if (temp > max) max = temp;                // 최고점은 변수 max에, 총점은 변수 sum에 저장하기
+            sum += temp;
+
+//            if (A[i] > max) max = A[i];
+//            sum = sum + A[i];
         }
         System.out.println(sum * 100.0 / max / N);      // sum * 100 / max / N 출력하기
     }
